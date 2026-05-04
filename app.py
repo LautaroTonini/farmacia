91e76ac6c0843c8badf1f734fa9bd11469241994
LautaroTonini <lautarotonini@gmail.com>
2026-05-04 17:48:38 -0300

    primero

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config.config import DATABASE_CONNECTION_URI
+from routes.client_routes import client
+from routes.products_routes import products
+from models.db import db
+
+app = Flask(__name__)
+
+app.register_blueprint(client)
+app.register_blueprint(products)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    from models.client import Client
+    from models.products import Products
+    # db.drop_all()
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ routes/client_route.py
@@ -0,0 +1,13 @@
+from sqlalchemy.exc import IntegrityError
+from flask import Blueprint, jsonify, request
+from models.db import db
+from models.client import Client
+
+client = Blueprint('client', __name__)
+
+
+
+@client.route('/api/clients')
+def get_client():
+    clients = Client.query.all()
+    return jsonify([client.serialize() for client in clients])
